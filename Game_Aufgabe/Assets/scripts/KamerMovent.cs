646f0047f04d219eacc57ff884a762c5d0b4538e
TheBroGamersTv <superdante34@gmail.com>
2018-04-27 23:50:07 +0200

    playertest
    +bugs

---
+++ Game_Aufgabe/Assets/scripts/KamerMovent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KamerMovent : MonoBehaviour {
+    public Transform playerpos;
+
+    public bool FirstPerson = false;
+    public Vector3 thirdview;
+    public Vector3 firstview;
+    public Quaternion playerlook;
+    
+	
+	// Update is called once per frame
+	void Update () {
+        playerlook = playerpos.rotation;
+        if (FirstPerson==true)
+        {
+           
+            transform.position = playerpos.position + thirdview;
+        }
+        else
+        {
+            transform.position = playerpos.position + firstview;
+        }
+        transform.rotation = playerlook;
+    }
+}
