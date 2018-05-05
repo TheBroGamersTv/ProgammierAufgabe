88c47e527872d6a7930d17a2bec6b825ef37742e
TheBroGamersTv <superdante34@gmail.com>
2018-05-05 14:11:14 +0200

    playermovent no bugs
    adding camera view , player controller, funktioning, jumpad +bugs

---
+++ Game_Aufgabe/Assets/scripts/PlayerContoller.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+
+    public float movementspeed = 2f;
+    public float RotationspeedX = 2f;
+    public float RotationspeedY = 2f;
+    public float negativgravityforce = 4f;
+    public float gravity = 5f;
+    CharacterController player;
+    public bool INvertedAXis = false;
+    public bool Jumping = false;
+    public GameObject Camera;
+    float moveforwbackw;
+    float movesideways;
+    float upforce;
+
+    float LOOKX;
+    float LOOKY;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = this.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Movement();
+
+      
+        if (Input.GetButtonDown("JUMP"))
+        {
+            Jumping = true;
+        }
+        JUMP();
+    }
+
+    public void JUMP()
+    {
+        if (player.isGrounded)
+        {
+            if (Jumping == false)
+            {
+                upforce = (-gravity);
+            }
+            else
+            {
+                upforce = negativgravityforce;
+            }
+        }
+        else
+        {
+            upforce += (-gravity) * Time.deltaTime;
+            upforce = Mathf.Clamp(upforce, -50, negativgravityforce);
+            Jumping = false;
+        }
+    }
+
+
+    void Movement()
+    {
+        movesideways = Input.GetAxis("Horizontal") * movementspeed;
+        moveforwbackw = Input.GetAxis("Vertical") * movementspeed;
+        Vector3 movement = new Vector3(movesideways, upforce, moveforwbackw);
+
+        LOOKX = Input.GetAxis("Mouse X") * RotationspeedX;
+        LOOKY = Input.GetAxis("Mouse Y") * RotationspeedY;
+        transform.Rotate(0, LOOKX, 0);
+        if (INvertedAXis)
+        {
+            Camera.transform.Rotate(-LOOKY, 0, 0);
+        }
+        else
+        {
+            Camera.transform.Rotate(LOOKY, 0, 0);
+        }
+        movement = transform.rotation * movement;
+        player.Move(movement * Time.deltaTime);
+    }
+}
---
+++ Game_Aufgabe/Assets/scripts/jumppadscript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumppadscript : MonoBehaviour {
+
+    public PlayerContoller player;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerContoller>();
+    }
+
+    // Update is called once per frame
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.Jumping = true;
+            player.JUMP();
+        }
+    }
+}
