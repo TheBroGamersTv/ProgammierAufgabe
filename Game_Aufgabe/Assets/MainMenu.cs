c7e287bbc362ae3d301d067952a7169da72fea92
Ericson <eric.micromega@gmail.com>
2018-05-18 17:58:28 +0200

    Options + Menü (Bugged)
    Fixing

---
+++ Game_Aufgabe/Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void StartGame()
+    {
+        //SceneManager.LoadScene("Game_Aufgabe.unity");
+        Debug.Log("Starting to load scene");
+    }
+  
+	public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quiting Game...");
+    }
+}
---
+++ Game_Aufgabe/Assets/OptionsMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour {
+
+    public AudioMixer SetVolume; //Connected with the AudioMixer
+    
+	public void Volume(float v)
+    {
+        Debug.Log(v);
+        SetVolume.SetFloat("volume", v);
+    }
+    public void Resolution()
+    {
+
+    }
+    public void Graphics()
+    {
+
+    }
+}
